071662ccf7985e1e9639deaf67e552dfd0a40414
govind <govindpurty350@gmail.com>
2025-11-23 01:32:48 +0530

    update on auth type

--- src/app/types/types-for-auth.ts
+++ src/app/types/types-for-auth.ts
@@ -1,20 +1,11 @@
-export interface CreateUserByRegisterRequest {
-  name: string;
-  email: string;
-  password: string;
-}
-
-export interface GetUserByLoginRequest {
-  email: string;
-  password: string;
-}
-
 export interface AuthResponse {
   status: string;
   message: string;
   data?: {
-    id: string;
-    email: string;
-    name?: string;
+    user: {
+      id: string;
+      email: string;
+      name?: string;
+    };
   } | null;
 }
